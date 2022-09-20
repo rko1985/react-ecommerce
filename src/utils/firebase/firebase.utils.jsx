8c862228b4a178a35f2f4edd4c8d10d68f6f9f3e
Russell Oyama <russelloyama@gmail.com>
2022-09-20 08:28:35 -1000

    finish creating user docs

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -39,10 +39,22 @@ const firebaseConfig = {
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef);
-
     const userSnashot = await getDoc(userDocRef);
 
-    console.log(userSnashot);
-    console.log(userSnashot.exists());
+    if(!userSnashot.exists()){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message);
+      }
+    }
+
+    return userDocRef;
   }
