74ec75894d63d86515c80a3692f63bd768253f3b
Russell Oyama <russelloyama@gmail.com>
2022-09-28 13:55:15 -1000

    finishing sign in form

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -4,7 +4,8 @@ import {
   signInWithRedirect, 
   signInWithPopup, 
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
   } 
 from 'firebase/auth';
 
@@ -70,5 +71,13 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+    
+  }
+
+  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
     
   }
