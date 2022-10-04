43783f3c56ca74fe6bfc2fa3963e00f91260376e
Russell Oyama <russelloyama@gmail.com>
2022-10-04 07:27:51 -1000

    signing out user

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
 
 import './sign-up-form.styles.scss';
 
@@ -17,9 +19,9 @@ const defaultFormFields = {
 const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
-    const { displayName, email, password, confirmPassword } = formFields;
+    const { displayName, email, password, confirmPassword } = formFields;  
     
-    console.log(formFields);
+    const { setCurrentUser } = useContext(UserContext)
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -35,6 +37,8 @@ const SignUpForm = () => {
 
         try {
             const {user} = await createAuthUserWithEmailAndPassword(email, password);
+
+            setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -5,7 +5,8 @@ import {
   signInWithPopup, 
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut
   } 
 from 'firebase/auth';
 
@@ -81,3 +82,5 @@ const firebaseConfig = {
     return await signInWithEmailAndPassword(auth, email, password);
     
   }
+
+  export const signOutUser = async () => await signOut(auth);
