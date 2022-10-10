e9fd8976a2c029df4a594fa06722f98041145066
Russell Oyama <russelloyama@gmail.com>
2022-10-10 10:23:26 -1000

    shop page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,8 @@ import { Routes, Route} from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-
-const Shop = () => {
-
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-    </div>
-    
-  );
-}
 
 const App = () =>{
   
