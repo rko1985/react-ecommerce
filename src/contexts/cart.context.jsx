0ff7b32cdf7a7638a47b37f27df6c7be1e38d6fb
Russell Oyama <russelloyama@gmail.com>
2022-10-24 11:31:02 -1000

    increment, decrement and remove item from checkout

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -35,12 +35,17 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
 
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => {    
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);    
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0
 });
 
@@ -62,7 +67,19 @@ export const CartProvider = ({children}) => {
         setCartItems(removeCartItem(cartItems,cartItemToRemove))
     };
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount};
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems,cartItemToClear))
+    };
+
+    const value = {
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart, 
+        removeItemToCart,
+        clearItemFromCart, 
+        cartItems, 
+        cartCount
+    };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -8,7 +8,7 @@ import './checkout.styles.scss';
 
 const Checkout = () => {
 
-    const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
+    const { cartItems } = useContext(CartContext);
 
     return (
         <div className='checkout-container'>
