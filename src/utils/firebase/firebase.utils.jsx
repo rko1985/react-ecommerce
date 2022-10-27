115333fdad9357a8eb6b185316e5667951af33b9
Russell Oyama <russelloyama@gmail.com>
2022-10-27 09:29:03 -1000

    uploading products to db

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -43,9 +43,17 @@ const firebaseConfig = {
 
   export const db = getFirestore();
 
-  export const AddCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    })
     
+    await batch.commit();
+    console.log('done');
   }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
