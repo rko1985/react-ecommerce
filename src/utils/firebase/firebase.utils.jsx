4d51232f2124764582b21de1db3f37c09c40f4b3
Russell Oyama <russelloyama@gmail.com>
2022-10-28 09:31:12 -1000

    get products from firebase

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -18,6 +18,8 @@ import {
   setDoc,
   collection,
   writeBatch,
+  query,
+  getDocs
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -54,6 +56,21 @@ const firebaseConfig = {
     
     await batch.commit();
     console.log('done');
+  };
+
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const {title, items} = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    },{});
+
+    return categoryMap;
+
   }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
