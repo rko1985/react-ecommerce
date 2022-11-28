427183182189c514342c56cf265f1edbfe1d3bf5
Russell Oyama <russelloyama@gmail.com>
2022-11-28 07:59:57 -1000

    business logic in selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,15 +5,15 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.actions';
+import { setCategories } from '../../store/categories/category.actions';
 
 const Shop = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-        const categoryMap = await getCategoriesAndDocuments();            
-        dispatch(setCategoriesMap(categoryMap));
+        const categoriesArray = await getCategoriesAndDocuments();
+        dispatch(setCategories(categoriesArray));
       };
       getCategoriesMap();
     },[]);  
