7c1d99f4d195075850fb7b8a24972d197236ef12
Russell Oyama <russelloyama@gmail.com>
2022-12-09 07:36:00 -1000

    creating loading spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,14 +4,16 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [ products, setProducts ] = useState(categoriesMap[category]);
     
     useEffect(() => {
@@ -21,12 +23,15 @@ const Category = () => {
     return(
         <Fragment>
             <Title>{category.toUpperCase()}</Title>
-            <CategoryContainer>            
+            {
+                isLoading ? <Spinner /> : <CategoryContainer>            
                 {products && 
                     products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
-            </CategoryContainer>
+                </CategoryContainer>
+            }
+            
         </Fragment>
     )
 }
