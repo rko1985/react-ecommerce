e07343de7a9acc4bd8d9fc35047a75112127ee32
Russell Oyama <russelloyama@gmail.com>
2022-12-14 07:25:48 -1000

    converting onAuthStateChanged listener to promise

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 
 import { Routes, Route} from 'react-router-dom';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { onAuthStateChangedListener, createUserDocumentFromAuth, getCurrentUser } from "./utils/firebase/firebase.utils";
 
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
@@ -17,14 +17,7 @@ const App = () =>{
   const dispatch = useDispatch();
   
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-        if(user){
-            createUserDocumentFromAuth(user);
-        }
-        dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, []);
 
   return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -113,3 +113,16 @@ const firebaseConfig = {
   export const signOutUser = async () => await signOut(auth);
 
   export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+  export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = onAuthStateChanged(
+        auth,
+        (userAuth) => {
+          unsubscribe();
+          resolve(userAuth);
+        },
+        reject
+      );
+    });
+  };
