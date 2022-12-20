d2ef93cfe4c925286622798e9661d7f7ed83ba67
Russell Oyama <russelloyama@gmail.com>
2022-12-19 14:45:19 -1000

    sign up saga

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -7,6 +8,7 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "
 
 
 import { SignUpContainer } from './sign-up-form.styles';
+import { signUpStart } from "../../store/user/user.action";
 
 const defaultFormFields = {
     displayName: '',
@@ -19,6 +21,7 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;  
+    const dispatch = useDispatch();
     
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -33,10 +36,7 @@ const SignUpForm = () => {
         }
 
         try {
-            const {user} = await createAuthUserWithEmailAndPassword(email, password);
-
-
-            await createUserDocumentFromAuth(user, {displayName});
+            dispatch(signUpStart(email, password, displayName));
             resetFormFields();
             
         } catch (error) {
