43209b8e67931208bd0067a9d71a847775ccac74
Russell Oyama <russelloyama@gmail.com>
2023-01-05 09:44:37 -1000

    typing category selector

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,4 +1,12 @@
-import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+
+import { CategoryAction } from "./category.actions";
+
+export type CategoriesState = {
+    readonly categories: Category[];
+    readonly isLoading: boolean;
+    readonly error: Error | null;
+};
 
 export const CATEGORIES_INITIAL_STATE = {
     categories: [],
@@ -6,15 +14,15 @@ export const CATEGORIES_INITIAL_STATE = {
     error: null,
 };
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action) => {
-    const { type, payload} = action;
-    switch(type) {
+export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as CategoryAction) => {
+    
+    switch(action.type) {
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
             return {...state, isLoading: true};
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-            return {...state, categories: payload, isLoading: false};
+            return {...state, categories: action.payload, isLoading: false};
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-            return {...state, error: payload, isLoading: false};
+            return {...state, error: action.payload, isLoading: false};
         default:
             return state;
     }
