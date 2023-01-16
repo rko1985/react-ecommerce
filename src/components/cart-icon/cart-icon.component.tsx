52e47f34e5321723394377a39db715cc39a6d97e
Russell Oyama <russelloyama@gmail.com>
2023-01-16 10:02:39 -1000

    typing SVG imports

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -3,7 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
 
-import {ShoppingIcon, CartIconContainer, ItemCount} from './cart-icon.styles';
+import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+
+import {CartIconContainer, ItemCount} from './cart-icon.styles';
 
 const CartIcon = () => {    
     const dispatch = useDispatch();
